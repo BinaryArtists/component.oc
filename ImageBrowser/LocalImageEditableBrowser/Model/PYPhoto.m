87d1f0a2eb9a0d4a7ca0f5fdbf3ac6b233a95aba
fallen <fengzilijie@gmail.com>
2017-03-14 12:24:02 +0800

    first commit

---
+++ ImageBrowser/LocalImageEditableBrowser/Model/PYPhoto.m
@@ -0,0 +1,13 @@
+#import "_precompile.h"
+#import "PYPhoto.h"
+
+@implementation PYPhoto
+
+- (instancetype)init {
+    if (self = [super init]) {
+        self.verticalWidth = screen_width;
+    }
+    return self;
+}
+
+@end
